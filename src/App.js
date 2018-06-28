245e59cd446463e079cdc0bac2575604830a1907
Alec <alecdifederico@gmail.com>
2018-06-27 21:42:40 -0400

    refactored components

--- src/App.js
+++ src/App.js
@@ -22,19 +22,30 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const apiCall = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`
-    );
-    const data = await apiCall.json();
+    if (city && country) {
+      const apiCall = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`
+      );
+      const data = await apiCall.json();
 
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ''
-    });
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'please enter the values.'
+      })
+    }
   }
 
   render() {
